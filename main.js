a90604be94bc57d3a870eba5625043b11542c077
hyowon <khw0107799@naver.com>
2021-09-03 13:35:01 +0900

    번경

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></meta>
+<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <title>홈페이지 만들기</title>
 </head>
